2c421a223a40362f717091380b5caecdeb0a0972
TomiSakae <tomisakaeanime@gmail.com>
2024-09-11 11:19:34 +0700

    fix

--- src/app/pptx/page.tsx
+++ src/app/pptx/page.tsx
@@ -9,7 +9,7 @@ function PPTXContent() {
 
     useEffect(() => {
         const name = searchParams.get('name') || 'output.pptx';
-        const newUrl = `https://deepnote.com/workspace/tomisakae-59909f41-bc0a-457b-92a3-3919af2aa9f2/project/Tomi-Sakaes-Untitled-project-c1bc4da6-34a0-43a6-ae76-ae3e33de7586/${name}?secondary-sidebar=app`;
+        const newUrl = `https://deepnote.com/workspace/tomisakae-59909f41-bc0a-457b-92a3-3919af2aa9f2/project/Tomi-Sakaes-Untitled-project-c1bc4da6-34a0-43a6-ae76-ae3e33de7586/output/${name}?secondary-sidebar=app`;
         router.push(newUrl);
     }, [searchParams, router]);
 
@@ -18,7 +18,7 @@ function PPTXContent() {
 
 export default function PPTXPage() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div></div>}>
             <PPTXContent />
         </Suspense>
     );
