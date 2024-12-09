03c21622c7eba769043822b6ae284bb9739262b0
TomiSakae <tomisakaeanime@gmail.com>
2024-12-09 11:53:14 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    // Thêm cấu hình để bỏ qua route API khi build
+    images: {
+        unoptimized: true,
+    },
+    // Bỏ qua các route API trong quá trình build
+    typescript: {
+        ignoreBuildErrors: true,
+    }
+};
 
 export default nextConfig;
